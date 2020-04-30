741b18337372fd611f27cdc658815917f09886d0
Shane <shanemcneil19@gmail.com>
2020-04-30 09:05:05 -0700

    complete remove duplicates string

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -2,25 +2,28 @@ const HashMap = require('./hashMap');
 
 function removeDuplicates(string) {
     let checkMap = new HashMap();
-
     let stringArr = string.split('');
-    // console.log(stringArr);
     for (let i = 0; i < stringArr.length; i++) {
+        if (stringArr[i] === ' ') {
+            continue;
+        }
         checkMap.set(stringArr[i], stringArr[i]);
     }
-    // console.log(checkMap);
-    // console.log(checkMap);
     let newArr = [];
     for (let i = 0; i < stringArr.length; i++) {
         let letter = stringArr[i];
+        if (letter === ' ') {
+            newArr.push(letter);
+        }
         let index = checkMap._findSlot(letter);
-        console.log('letter', letter, 'index', index, 'checkmap[index]', checkMap._hashTable[index]);
         if (checkMap._hashTable[index]) {
             newArr.push(letter);
             checkMap.delete(letter);
         }
     }
-    console.log(newArr);
+    let noDuplicateString = newArr.join('');
+    return noDuplicateString;
 }
 
-removeDuplicates('google');
+console.log(removeDuplicates('google'));
+console.log(removeDuplicates('google all that you think can think of'));
